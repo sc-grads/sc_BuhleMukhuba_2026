dfdbd519cf95fc1deb926d30b750c89d2934178e
Buhle Mukhuba <buhle.mukhuba@sambeconsulting.com>
2026-04-01 17:53:44 +0200

    Completed sections 1&2, committing script, simple adder and mad libs

---
+++ Python for Beginners/main.py
@@ -0,0 +1,29 @@
+x : int = 2
+x += 2
+print(x)
+x -= 2
+print(x)
+x *= 2
+print(x)
+x /= 2
+print(x)
+
+a: int = 1
+b: int = 5
+c: int = 10
+d: int = 10
+
+print(a == b)
+print(c == d)
+print(a != b)
+
+print(b > a)
+print(a < b)
+print(c > d)
+print(c >= d)
+
+print(c > b > a)
+print(c > b and d > a)
+print(c > b or d > a)
+
+
