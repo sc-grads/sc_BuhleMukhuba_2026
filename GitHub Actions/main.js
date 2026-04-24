98c7dfed9193ad62b9e433fe090c247f3c06b02f
Buhle Mukhuba <buhle.mukhuba@sambeconsulting.com>
2026-04-24 15:53:42 +0200

    Committing custom actions section

---
+++ GitHub Actions/main.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
+
+function run(){
+    // 1) Get some inputs
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
+
+    //2) Upload files
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync <local-folder> ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    const websiteURL = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl); 
+}
+
+run();
